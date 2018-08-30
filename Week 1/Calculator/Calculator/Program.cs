d5bdf971a88e9b436c697c3f3deaeabbac87e472
christoffer jakobsen <christoffer.k.jakobsen@gmail.com>
2018-08-30 09:26:50 +0200

    Week 1 added

---
+++ Week 1/Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Methods.Calculator tak = new Methods.Calculator();
+
+            Console.WriteLine(tak.Add(5,6)); 
+            Console.WriteLine(tak.Subtract(6,2));
+            Console.WriteLine(tak.Multiply(5,5));
+            Console.WriteLine(tak.Power(5,5));
+
+        }
+
+    
+    }
+}
