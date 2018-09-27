77d418ee2f9ff9eac0c4d652c7f8e9c0b2c351e4
christoffer jakobsen <christoffer.k.jakobsen@gmail.com>
2018-09-27 11:11:38 +0200

    Cool

---
+++ AirTrafficMonitor/AirTrafficMonitor/AirTrafficMonitor/ATM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitor
+{
+
+
+    namespace TransponderReceiver
+    {
+        public class RawTransponderDataEventArgs : EventArgs
+        {
+            public RawTransponderDataEventArgs(List<string> transponderData)
+            {
+                TransponderData = transponderData;
+            }
+            public List<string> TransponderData { get; }
+        }
+        public interface ITransponderReceiver
+        {
+            event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
+        }
+
+    }
+
+    class ATM
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TransponderReceiver.RawTransponderDataEventArgs(););
+
+        }
+
+
+    }
+}
