5342d36e282096aefe54d0b4a77ddad94029c0b8
christoffer jakobsen <christoffer.k.jakobsen@gmail.com>
2018-09-28 11:17:09 +0200

    ved ikke

---
+++ ATM/ATM rendition/Rendition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Rendition
+    {
+        static void Main()
+        {
+
+            List<string> data = new List<string>();
+ 
+            string fly1 = "ATR423;39045;12932;14000;20151006213456789";
+            string fly2 = "ATR999;40000;12932;14000;20151006213456789";
+
+            data.Add(fly1);
+            data.Add(fly2);
+
+           
+
+
+            Console.WriteLine(Decoder.Decode(data).Tag, Decoder.Decode(data).TimeStamp);
+
+           
+
+        }
+    }
+}
---
+++ ATM/AirTrafficMonitor/Classes/Decoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ATM
+{
+    public class Decoder
+    {
+        public static Plane Decode(List<string> ddata)
+        {
+
+            foreach (string fly in ddata)
+            {
+
+                string[] split = fly.Split(';');
+
+                string tag = split[0];
+                string X_Coord = split[1];
+                string Y_Coord = split[2];
+                string Alt = split[3];
+                string Timestamp = split[4];
+
+                Plane flyet = new Plane(tag, X_Coord, Y_Coord, Alt, Timestamp);
+
+                return flyet;
+
+            }
+            return null; 
+        }
+
+    }
+}
+
