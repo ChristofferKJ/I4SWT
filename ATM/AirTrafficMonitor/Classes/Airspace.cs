054fd9e34c087c33a266c974f988cdc99d95923a
Hindkaer <hindkaer_msn@hotmail.com>
2018-10-02 10:15:11 +0200

    Airspaceklasse tilfoejet

---
+++ ATM/AirTrafficMonitor/Classes/Airspace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ATM
+{
+    public class Airspace
+    {
+        public List<Plane> Planes { get; set; }
+
+        public Airspace()
+        {
+            const int SouthWest_Y = 10000;
+            const int SouthWest_X = 10000;
+            const int NorthEast_Y = 90000;
+            const int NorthEast_X = 90000;
+            const int MaxHeight = 20000;
+            const int MinHeight = 500;
+        }
+
+
+    }
+}
