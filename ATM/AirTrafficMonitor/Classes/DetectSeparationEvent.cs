c584752631ad9a8031819cda766ce377e5978adb
JohnTraDolta <mariusslmoeller@gmail.com>
2018-10-02 10:59:29 +0200

    Marius tilføjer

---
+++ ATM/AirTrafficMonitor/Classes/DetectSeparationEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{ 
+    class DetectSeparationEvent : IDetectSeparationEvent
+    {
+        public void CheckSepEvent(List<Plane> planes)
+        {
+            foreach (Plane plane1 in planes)
+            {
+                foreach (Plane plane2 in planes)
+                {
+                    if (plane1.XCoordinate - plane2.XCoordinate < 5000 || 
+                        plane1.YCoordinate - plane2.YCoordinate < 5000 ||
+                        plane1.Altitude - plane2.Altitude < 300)
+                    {
+                        PassSepEvent(plane1, plane2, plane1.TimeStamp);
+                    }
+                }
+            }
+        }
+        void PassSepEvent(Plane plane1, Plane plane2, string timestamp)
+        {
+            Log log = new Log();
+            log.WriteToLog(plane1.Tag, plane2.Tag, timestamp);
+        }
+    }
+}
