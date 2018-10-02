c44f319904a04df9fa1815054dee2ced30d0b4b4
Hindkaer <hindkaer_msn@hotmail.com>
2018-10-02 12:58:16 +0200

    Opdateringer til Airspace klassen

--- ATM/AirTrafficMonitor/Classes/Airspace.cs
+++ ATM/AirTrafficMonitor/Classes/Airspace.cs
@@ -10,17 +10,27 @@ namespace ATM
     public class Airspace
     {
         public List<Plane> Planes { get; set; }
+        public const int SouthWest_Y = 10000;
+        public const int SouthWest_X = 10000;
+        public const int NorthEast_Y = 90000;
+        public const int NorthEast_X = 90000;
+        public const int MaxHeight = 20000;
+        public const int MinHeight = 500;
 
         public Airspace()
         {
-            const int SouthWest_Y = 10000;
-            const int SouthWest_X = 10000;
-            const int NorthEast_Y = 90000;
-            const int NorthEast_X = 90000;
-            const int MaxHeight = 20000;
-            const int MinHeight = 500;
         }
 
+        public void Checkairspace(Airspace airspace)
+        {
+            foreach (var plane in Planes)
+            {
+                if (plane.XCoordinate < SouthWest_X || plane.XCoordinate < SouthWest_Y || plane.YCoordinate > NorthEast_X || plane.YCoordinate >NorthEast_Y || plane.Altitude < MinHeight || plane.Altitude > MaxHeight)
+                {
+                    Planes.Remove(plane);
+                }
+            }
+        }
 
     }
 }
