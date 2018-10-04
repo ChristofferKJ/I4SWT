9d18785c19b21f63d390b1659ed414e4bf368621
christoffer jakobsen <christoffer.k.jakobsen@gmail.com>
2018-10-04 15:58:11 +0200

    hey

--- ATM/AirTrafficMonitor.Unit.Test/Tests/DetectSeparationEventTest.cs
+++ ATM/AirTrafficMonitor.Unit.Test/Tests/DetectSeparationEventTest.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using NUnit.Framework.Internal;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NUnit.Framework;
+//using NUnit.Framework.Internal;
 
-namespace AirTrafficMonitor.Unit.Test.Tests
-{
-    [TestFixture]
-    class DetectSeparationEventTest
-    {
-        [SetUp]
+//namespace AirTrafficMonitor.Unit.Test.Tests
+//{
+//    [TestFixture]
+//    class DetectSeparationEventTest
+//    {
+//        [SetUp]
 
-    }
-}
+//    }
+//}
